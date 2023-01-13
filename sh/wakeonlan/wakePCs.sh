3a1e0aad7ea7c4f85063a21f6571571d9c92202c
Ryan H <dreded@Dredx.com>
2023-01-12 22:58:06 -0800

    added ability to find its own folder if symlinked

--- sh/wakeonlan/wakePCs.sh
+++ sh/wakeonlan/wakePCs.sh
@@ -14,7 +14,15 @@ timeout=10
 #   helpful when running from desktop shortcut so you can see the output
 delay_before_close=3
 
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPT_DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
 
+mac_file=$SCRIPT_DIR/$mac_file
 wakepc()
 {
     found=true
